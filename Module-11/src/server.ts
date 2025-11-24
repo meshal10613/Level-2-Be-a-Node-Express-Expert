43fae06a029a4e343b9bcd3b3bbfadfeb26bdb3c
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-11-24 18:00:12 +0600

    module 11, vedio 3, done!

--- Module-11/src/server.ts
+++ Module-11/src/server.ts
@@ -11,11 +11,49 @@ const server: Server = http.createServer(
                     path: req.url,
                 })
             );
-        };
+        }
+
+        //? health route
+        if (req.url === "/api" && req.method === "GET") {
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(
+                JSON.stringify({
+                    message: "Health Status OK!",
+                    path: req.url,
+                })
+            );
+        }
+
+        if (req.url === "/api/users" && req.method === "POST") {
+            // const user = {
+            //     id: 1,
+            //     name: "Alice",
+            // };
+            // res.writeHead(200, { "Content-Type": "application/json" });
+            // res.end(JSON.stringify(user));
+
+            let body = "";
+            // listen for data chunk
+            req.on("data", (chunk) => {
+                body += chunk.toString();
+            });
+
+            req.on("end", () => {
+                try {
+                    const parseBody = JSON.parse(body);
+                    res.end(JSON.stringify(parseBody));
+                } catch (error: any) {
+					res.end(error.message);
+				}
+            });
+        }
+
+        if (req.url === "/" && req.method === "POST") {
+            req.on("data", (chunk) => {});
+        }
     }
 );
 
-
 server.listen(config.port, () => {
-	console.log(`Server is running on port ${config.port}`);
-})
+    console.log(`Server is running on port ${config.port}`);
+});
