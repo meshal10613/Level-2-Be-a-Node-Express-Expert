2cf57e815c4901c67d1898aa93adc6f2d1feea48
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-11-24 17:36:10 +0600

    module 11, vedio 2, done!

--- Module-11/src/config/index.ts
+++ Module-11/src/config/index.ts
@@ -1,6 +1,11 @@
-import dotenv, { config } from "dotenv";
+import dotenv from "dotenv";
 import path from "path";
 
 dotenv.config({path: path.join(process.cwd(), ".env")});
 
-export default config
+const config = {
+	env: process.env.NODE_ENV || "development",
+	port: process.env.PORT || 5000,
+};
+
+export default config;
