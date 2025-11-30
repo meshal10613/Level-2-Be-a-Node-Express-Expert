acd8a240c0c4cf33c2783a545d5c563cf37d1b75
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-11-30 18:32:32 +0600

    module 12, vedio 4, todos pool created

--- Module-12/src/config/index.ts
+++ Module-12/src/config/index.ts
@@ -1,8 +1,13 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({path: path.join(process.cwd(), ".env")});
+
 const config = {
 	app: {
 		port: process.env.PORT || 5000,
 		env: process.env.NODE_ENV || "development",
-		psql_string: process.env.PSQL_CONNECTION_STRING
+		psql_string: process.env.PSQL_STRING
 	}
 };
 
--- Module-12/src/server.ts
+++ Module-12/src/server.ts
@@ -1,15 +1,13 @@
-import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import { Pool } from "pg";
 import config from "./config";
 
-dotenv.config();
 const app = express();
 const port = config.app.port;
 
 //? middleware
 app.use(express.json()); // for json data
-app.use(express.urlencoded()); // for form data
+app.use(express.urlencoded({ extended: true })); // for form data
 
 const pool = new Pool({
     connectionString: config.app.psql_string,
@@ -24,9 +22,22 @@ const initDB = async () => {
 			age INT,
 			phone VARCHAR(20),
 			address TEXT,
-			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-			updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-			)
+			created_at TIMESTAMP DEFAULT NOW(),
+			updated_at TIMESTAMP DEFAULT NOW()
+		)
+	`);
+
+	await pool.query(`
+		CREATE TABLE IF NOT EXISTS todos (
+			id SERIAL PRIMARY KEY,
+			user_id INT REFERENCES users(id) ON DELETE CASCADE,
+			title VARCHAR(255) NOT NULL,
+			description TEXT,
+			completed BOOLEAN DEFAULT false,
+			due_date DATE,
+			created_at TIMESTAMP DEFAULT NOW(),
+			updated_at TIMESTAMP DEFAULT NOW()
+		)
 	`);
 };
 
