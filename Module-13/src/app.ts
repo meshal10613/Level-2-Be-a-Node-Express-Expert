33676516648d914bfd545ebfd9c96b9ebcac566d
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-12-04 11:23:53 +0600

    module 13, done!

---
+++ Module-13/src/app.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from "express";
+import initDB from "./config/db";
+import logger from "./middleware/logger";
+import { userRoutes } from "./modules/user/user.routes";
+import { todoRoutes } from "./modules/todo/todo.routes";
+import { authRoutes } from "./modules/auth/auth.routes";
+
+const app = express();
+
+//? middleware
+app.use(express.json()); // for json data
+app.use(express.urlencoded({ extended: true })); // for form data
+
+//? initialise DB
+initDB();
+
+app.get("/", logger, async (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+app.use("/users", userRoutes);
+app.use("/todos", todoRoutes);
+app.use("/auth", authRoutes);
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({
+        path: req.url,
+        success: false,
+        message: "Not Found!",
+    });
+});
+
+export default app;
