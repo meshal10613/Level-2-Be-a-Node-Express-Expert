cb5c32788e2a0901d3819550dc7eb1ebfa520002
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-12-04 10:55:15 +0600

    module 13, vedio 11, jwt verify done

--- Module-13/src/middleware/auth.ts
+++ Module-13/src/middleware/auth.ts
@@ -1,16 +1,25 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config";
 
 const auth = () => {
     return (req: Request, res: Response, next: NextFunction) => {
-        const token = req.headers.authorization;
-        if (!token) return res.status(401).json({ message: "Unauthorized Access" });
+        try {
+            const token = req.headers.authorization;
+            if (!token)
+                return res.status(401).json({ message: "Unauthorized Access" });
 
-		const decoded = jwt.verify(token, config.app.jwt_secret as string);
-		console.log(decoded)
-
-        next();
+            const decoded = jwt.verify(token, config.app.jwt_secret as string);
+            req.user = decoded as JwtPayload;
+            next();
+        } catch (error: any) {
+            res.status(500).json({
+                path: req.url,
+                success: false,
+                message: error.message,
+                details: error,
+            });
+        }
     };
 };
 
